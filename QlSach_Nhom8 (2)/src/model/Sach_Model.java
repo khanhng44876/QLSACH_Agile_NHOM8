ebedae96c22862c3e404ab8fcb54b61b84fc0116
HoangDucIch2004 <hoangich140704@gmail.com>
2023-11-20 21:37:18 +0700

    Sprint5

---
+++ QlSach_Nhom8 (2)/src/model/Sach_Model.java
@@ -0,0 +1,96 @@
+
+package model;
+
+
+public class Sach_Model {
+    private String ma;
+    private String tenSach;
+    private String tenTacGia;
+    private String loaiSach;
+    private Integer soLuong;
+    private Integer nam;
+    private double gia;
+
+    public Sach_Model() {
+    }
+
+    public Sach_Model(String ma, String tenSach, String tenTacGia, String loaiSach, Integer soLuong, Integer nam, double gia) {
+        this.ma = ma;
+        this.tenSach = tenSach;
+        this.tenTacGia = tenTacGia;
+        this.loaiSach = loaiSach;
+        this.soLuong = soLuong;
+        this.nam = nam;
+        this.gia = gia;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getTenSach() {
+        return tenSach;
+    }
+
+    public void setTenSach(String tenSach) {
+        this.tenSach = tenSach;
+    }
+
+    public String getTenTacGia() {
+        return tenTacGia;
+    }
+
+    public void setTenTacGia(String tenTacGia) {
+        this.tenTacGia = tenTacGia;
+    }
+
+    public String getLoaiSach() {
+        return loaiSach;
+    }
+
+    public void setLoaiSach(String loaiSach) {
+        this.loaiSach = loaiSach;
+    }
+
+    public Integer getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(Integer soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public Integer getNam() {
+        return nam;
+    }
+
+    public void setNam(Integer nam) {
+        this.nam = nam;
+    }
+
+    public double getGia() {
+        return gia;
+    }
+
+    public void setGia(double gia) {
+        this.gia = gia;
+    }
+    
+    public Object[] toDataRow(){
+        return new Object[]{
+            this.ma, this.tenSach, this.tenTacGia, this.loaiSach, this.soLuong, this.nam, this.gia
+                
+        };
+    }
+
+    @Override
+    public String toString() {
+        return "Sach_Model{" + "ma=" + ma + ", tenSach=" + tenSach + ", tenTacGia=" + tenTacGia + ", loaiSach=" + loaiSach + ", soLuong=" + soLuong + ", nam=" + nam + ", gia=" + gia + '}';
+    }
+    
+           
+}
