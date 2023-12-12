6df593f09269ac608891daac846187a062ad27a7
lytienduy <133567310+lytienduy@users.noreply.github.com>
2023-12-12 20:17:10 +0700

    Add files via upload

---
+++ QL_Sach.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE QUAN_LY_SACH_NHOM8
+use QUAN_LY_SACH_NHOM8
+CREATE TABLE SACH(
+	MaSach varchar(10) not null PRIMARY KEY,
+	TenSach nvarchar(50) not null,
+	TenTacGia nvarchar(50) not null,
+	LoaiSach nvarchar(30) not null,
+	SoLuong int not null,
+	NamXuatBan int not null,
+	GiaSach float not null
+);
+
+create table ACCOUNT(
+	usename varchar(50) primary key not null,
+	pass varchar(50) not null,
+	confirm varchar(50) not null,
+)
+drop table HoaDon
+create table HoaDon(
+	MaHoaDon int identity(1,1) primary key not null,
+	MaSach varchar(10) not null,
+	TenSach nvarchar(50) not null,
+	Soluongmua int not null,
+	DiaChi nvarchar(50) not null,
+	SDT varchar(12) not null,
+	ThanhTien money,
+	foreign key(MaSach) references SACH(MaSach)
+)
+create table HoaDonChiTiet(
+	MaHoaDonChiTiet bigint identity(1,1) primary key not null,
+	MaSach varchar(10) not null,
+	TenSach nvarchar(50) not null,
+	Soluongmua int not null,
+	DiaChi nvarchar(50) not null,
+	SDT varchar(12) not null,
+	ThanhTien money,
+	HinhThucThanhToan nvarchar(50) not null,
+	foreign key(MaSach) references SACH(MaSach)
+
+)
+
+SELECT hd.MaHoaDon,s.MaSach,s.TenSach,hd.Soluongmua,hd.DiaChi,hd.SDT,(hd.Soluongmua * s.GiaSach) as ThanhTien FROM HoaDon hd join SACH s on s.MaSach = hd.MaSach
+SELECT MaSach,TenSach,Soluongmua,ThanhTien FROM HoaDonChiTiet
+
+SELECT
+    SACH.MaSach,
+    SACH.TenSach,
+    SUM(HoaDonChiTiet.Soluongmua) AS SoLuongDaBan,
+    SUM(HoaDonChiTiet.ThanhTien) AS TongThanhTien
+FROM
+    SACH
+JOIN
+    HoaDonChiTiet ON SACH.MaSach = HoaDonChiTiet.MaSach
+GROUP BY
+    SACH.MaSach,
+    SACH.TenSach;
